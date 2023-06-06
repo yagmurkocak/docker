575a876a43061f3c4f81b5a78b2e80d6b23a5e5d
yagmurkocak <s_kocak22@stud.hwr-berlin.de>
2023-06-06 20:22:30 +0200

    exercise

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project Description
+
+This project is a Flask web application that displays Titanic dataset information. It includes functionality to view the first 5 rows of the dataset, as well as a page that shows a bar chart depicting the number of male and female survivors.
+
+## Dependencies
+
+- Python 3.7+
+- Flask 2.3.2
+- Pandas
+- Matplotlib
+
+## Usage
+
+1. Install the necessary dependencies by running `pip install -r requirements.txt`.
+2. Run the application using `docker-compose up`.
+3. Access the web application at `http://localhost:5000`.
--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
 import time
 import redis
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv() 
+cache = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379,  password=os.getenv('REDIS_PASSWORD'))
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+
 
 def get_hit_count():
     retries = 5
@@ -20,6 +26,8 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return render_template('hello.html', name= "BIPM", count = count)
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
